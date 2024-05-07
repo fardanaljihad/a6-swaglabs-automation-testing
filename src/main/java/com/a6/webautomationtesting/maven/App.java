5e92993db5ce9b050ce234d618ffd273abc4c18b
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-07 11:54:18 +0700

    create project

---
+++ src/main/java/com/a6/webautomationtesting/maven/App.java
@@ -0,0 +1,13 @@
+package com.a6.webautomationtesting.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
