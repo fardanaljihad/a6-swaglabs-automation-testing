9585c245ae6ef144b456092a0adba0f56a803ff7
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-12 21:44:39 +0700

    create Step definition for Login.feature

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,31 +1,58 @@
 package io.cucumber.skeleton;
 
-import io.cucumber.java.en.When;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.And;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinitions {
-    private int firstNumber;
-    private int secondNumber;
-    private int result;
+    WebDriver driver;
 
-    @Given("I have entered {int} into the calculator")
-    public void i_have_entered_into_the_calculator(int number) {
-        if (firstNumber == 0) {
-            firstNumber = number;
-        } else {
-            secondNumber = number;
-        }
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/tools/chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
     }
 
-    @When("I press add")
-    public void i_press_add() {
-        result = firstNumber + secondNumber;
+    @When("I enter username {string} and password {string}")
+    public void iEnterUsernameAndPassword(String username, String password) {
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
     }
 
-    @Then("the result should be {int} on the screen")
-    public void the_result_should_be_on_the_screen(int expected) {
-        assert result == expected;
-    }
+    @And("I click the login button")
+    public void iClickTheLoginButton() {
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
     }
 
+    @Then("I should be logged in successfully")
+    public void iShouldBeLoggedInSuccessfully() {
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        String actualUrl = driver.getCurrentUrl();
+        assertEquals(expectedUrl, actualUrl);
+
+        driver.quit();
+    }
+
+    @Then("I should see an error message {string}")
+    public void iShouldSeeAnErrorMessage(String expectedErrorMessage) {
+        WebElement errorMessage = driver.findElement(By.cssSelector("[data-test='error']"));
+        String actualErrorMessage = errorMessage.getText();
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+
+        driver.quit();
+    }
+}
