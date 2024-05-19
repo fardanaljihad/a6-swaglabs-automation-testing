a5efc765ecf64a028c80d37736f373c2bfe6f9fc
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-19 14:13:53 +0700

    Implement Hooks

--- src/test/java/io/cucumber/swaglabs/steps/LoginSteps.java
+++ src/test/java/io/cucumber/swaglabs/steps/LoginSteps.java
@@ -2,34 +2,18 @@ package io.cucumber.swaglabs.steps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.openqa.selenium.WebDriver;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.swaglabs.pages.LoginPage;
-import io.cucumber.swaglabs.utils.DriverManager;
 
 public class LoginSteps {
-  WebDriver driver = DriverManager.getDriver();
-  LoginPage objLoginPage = new LoginPage(driver);
-
-  @Before
-  public void setUp() {
-    driver = DriverManager.getDriver();
-  }
-
-  @After
-  public void tearDown() {
-    DriverManager.closeDriver();
-  }
+  LoginPage objLoginPage = new LoginPage(Hooks.driver);
 
   @Given("I am on the login page")
   public void iAmOnTheLoginPage() {
-    driver.get("https://www.saucedemo.com/");
+    Hooks.driver.get("https://www.saucedemo.com/");
   }
 
   @When("I enter username {string} and password {string}")
@@ -46,7 +30,7 @@ public class LoginSteps {
   @Then("I should be logged in successfully")
   public void iShouldBeLoggedInSuccessfully() {
     String expectedUrl = "https://www.saucedemo.com/inventory.html";
-    String actualUrl = driver.getCurrentUrl();
+    String actualUrl = Hooks.driver.getCurrentUrl();
     assertEquals(expectedUrl, actualUrl);
   }
 
--- src/test/java/io/cucumber/swaglabs/steps/LogoutSteps.java
+++ src/test/java/io/cucumber/swaglabs/steps/LogoutSteps.java
@@ -2,35 +2,19 @@ package io.cucumber.swaglabs.steps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.openqa.selenium.WebDriver;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.swaglabs.pages.InventoryPage;
 import io.cucumber.swaglabs.pages.LoginPage;
-import io.cucumber.swaglabs.utils.DriverManager;
 
 public class LogoutSteps {
-  WebDriver driver = DriverManager.getDriver();
-  LoginPage objLoginPage = new LoginPage(driver);
-  InventoryPage objInventoryPage = new InventoryPage(driver);
-
-  @Before
-  public void setUp() {
-    driver = DriverManager.getDriver();
-  }
-
-  @After
-  public void tearDown() {
-    DriverManager.closeDriver();
-  }
+  LoginPage objLoginPage = new LoginPage(Hooks.driver);
+  InventoryPage objInventoryPage = new InventoryPage(Hooks.driver);
 
   @Given("I am has logged in with valid credentials")
   public void i_am_has_logged_in_with_valid_credentials() {
-    driver.get("https://www.saucedemo.com/");
+    Hooks.driver.get("https://www.saucedemo.com/");
     objLoginPage.enterUsername("standard_user");
     objLoginPage.enterPassword("secret_sauce");
     objLoginPage.clickLoginButton();
@@ -39,13 +23,18 @@ public class LogoutSteps {
   @Given("I am on the dashboard page")
   public void i_am_on_the_dashboard_page() {
     String expectedUrl = objInventoryPage.getUrl();
-    String actualUrl = driver.getCurrentUrl();
+    String actualUrl = Hooks.driver.getCurrentUrl();
     assertEquals(expectedUrl, actualUrl);
   }
 
   @When("I open the menu")
   public void i_open_the_menu() {
     objInventoryPage.openMenu();
+    try {
+      Thread.sleep(5000); // Wait for 5 seconds
+  } catch (InterruptedException e) {
+      e.printStackTrace();
+  }
   }
 
   @When("I click the logout button")
@@ -56,7 +45,7 @@ public class LogoutSteps {
   @Then("I should be logged out successfully")
   public void i_should_be_logged_out_successfully() {
     String expectedUrl = "https://www.saucedemo.com/";
-    String actualUrl = driver.getCurrentUrl();
+    String actualUrl = Hooks.driver.getCurrentUrl();
     assertEquals(expectedUrl, actualUrl);
   }
 }
